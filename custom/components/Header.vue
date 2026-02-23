bb3a22881e74db24ff8908dc9bb922dc1f33cd05
jonathan-martz <github@jmartz.de>
2026-02-23 15:48:01 +0100

    #wip - adjust header ofr mobile and desktop

--- custom/components/Header.vue
+++ custom/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="header w-full bg-secondary text-white mb-3 px-3 py-3">
+  <section class="header w-full bg-secondary text-white px-3 py-3" :class="{'mb-3': !open}">
     <div class="flex w-full justify-between mx-auto max-w-6xl">
-      <h1 class="text-xl">
+      <h1 class="text-xl flex items-center">
         <a href="/" class="font-bold">PocketCms.io</a>
       </h1>
       <nav class="desktop hidden md:inline">
@@ -15,6 +15,42 @@
           <li class=""><a href="/">Docs</a></li>
         </ul>
       </nav>
+      <nav class="md:hidden text-white">
+        <button v-if="!open" @click="open = !open" class="btn btn-sm btn-primary">
+          <FontAwesomeIcon :icon="['fas', 'bars']" />
+        </button>
+        <button v-else @click="open = !open" class="btn btn-sm btn-primary">
+          <FontAwesomeIcon :icon="['fas', 'times']" />
+        </button>
+      </nav>
     </div>
   </section>
+
+  <section v-if="open" :class="{'mb-3': open}" class="bg-white px-3 py-3">
+    <section class="grid grid-cols-6 gap-3">
+      <div class="col-span-3">
+        <a href="/" class="btn btn-block btn-sm btn-secondary">Startseite</a>
+      </div>
+      <div class="col-span-3">
+        <a href="/" class="btn btn-block btn-sm btn-secondary">Startseite</a>
+      </div>
+      <div class="col-span-3">
+        <a href="/demos" class="btn btn-block btn-sm btn-secondary">Demos</a>
+      </div>
+      <div class="col-span-3">
+        <a href="/impressum" class="btn btn-block btn-sm btn-secondary">Impressum</a>
+      </div>
+    </section>
+  </section>
 </template>
+
+<script setup lang="ts">
+import { useLocalStorage } from "@vueuse/core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+const open = useLocalStorage('mobile-nav', false, {});
+
+onMounted(()=>{
+  open.value = false;
+})
+</script>
